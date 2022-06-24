42e0779da7fce0acb88fc2d0f6210b63060eefaa
Tim Greller <timlg07@gmail.com>
2022-06-24 22:44:09 +0200

    switch from gradle to maven

---
+++ src/main/java/tk/programdream/challenges/floyd_warshall/Runner.java
@@ -0,0 +1,10 @@
+package tk.programdream.challenges.floyd_warshall;
+
+public class Runner {
+    public static void main(String[] args) {
+        FileParser fp = new FileParser("demo.txt");
+        fp.apply("A ----> B");
+        // breakpoint
+        return;
+    }
+}
