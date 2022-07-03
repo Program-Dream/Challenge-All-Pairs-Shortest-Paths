92bd958d8e8abc0929c22ac4534ec2c3cb84536b
Tim Greller <timlg07@gmail.com>
2022-07-03 18:09:00 +0200

    put filenames in constants

--- src/main/java/tk/programdream/challenges/floyd_warshall/Runner.java
+++ src/main/java/tk/programdream/challenges/floyd_warshall/Runner.java
@@ -4,21 +4,28 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class Runner {
+
+    private static final String INPUT_FILE_NAME = "demo.txt";
+    private static final String OUTPUT_FILE_NAME = "out.txt";
+
     public static void main(String[] args) {
-        FileParser fp = new FileParser("demo.txt");
+        FileParser fp = new FileParser(INPUT_FILE_NAME);
         try {
             fp.read();
         } catch (IOException | NumberFormatException e) {
             System.out.println("Unable to parse file");
             e.printStackTrace();
         }
+
         Graph g = fp.get();
+
         ShortestPathSolver solver = new ShortestPathSolver(g);
         solver.solve();
+
         PathWriter writer = new PathWriter(solver);
         try {
-            writer.writeFile("out.txt");
-        } catch (IOException | NumberFormatException e) {
+            writer.writeFile(OUTPUT_FILE_NAME);
+        } catch (IOException e) {
             System.out.println("Unable to write file");
             e.printStackTrace();
         }
