c7d056a1348b8e46c32a2c61123738da08cad2c3
Tim Greller <timlg07@gmail.com>
2022-07-04 14:20:26 +0200

    add demo file back to the repo

--- src/main/java/tk/programdream/challenges/floyd_warshall/Runner.java
+++ src/main/java/tk/programdream/challenges/floyd_warshall/Runner.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public class Runner {
 
-    private static final String INPUT_FILE_NAME = "demo.txt";
+    private static final String INPUT_FILE_NAME = "in.txt";
     private static final String OUTPUT_FILE_NAME = "out.txt";
 
     public static void main(String[] args) {
